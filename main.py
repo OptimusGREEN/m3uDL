12f80cb27706c4b28892ab83e1074e2d4370bce5
OptimusGREEN <johnmcbridegreen@gmail.com>
2023-03-29 21:04:30 +0100

    browse function added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
 from PySide6.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 
@@ -17,6 +17,8 @@ class MainWindow(QWidget):
 
         self.ui.pushButton_search.clicked.connect(self._search)
         self.ui.lineEdit_download_path.textChanged.connect(self._set_dl)
+        self.ui.toolButton_download_path.clicked.connect(self._browse_dl)
+        # self.ui.lineEdit_search.textChange.connect(self._search)
         # self.ui.lineEdit_url.setFocus()
 
     def load_ui(self):
@@ -28,10 +30,22 @@ class MainWindow(QWidget):
         print(self.ui.lineEdit_search.text())
 
     @Slot()
-    def _set_dl(self):
-        if self.ui.lineEdit_download_path.text():
-            self.dl_dir = self.ui.lineEdit_download_path.text()
+    def _set_dl(self, dl_dir=None):
+        if not dl_dir:
+            if self.ui.lineEdit_download_path.text():
+                self.dl_dir = self.ui.lineEdit_download_path.text()
+        else:
+            self.ui.lineEdit_download_path.setText(dl_dir)
+            self.dl_dir = dl_dir
         print("Download path set to: {}".format(self.dl_dir))
+
+    @Slot()
+    def _browse_dl(self):
+        dialog = QFileDialog(self, directory="~")
+        dialog.setWindowTitle("Select Download Directory")
+        selection = dialog.getExistingDirectory()
+        self._set_dl(selection)
+        # print(selection)
 
 
 
