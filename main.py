25d3b164ca72eb1540e9b49db29017b1d0939d5e
OptimusGREEN <johnmcbridegreen@gmail.com>
2023-04-01 23:19:14 +0100

    add search method

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import os
 from pathlib import Path
 import sys
+import logging
+import urllib.request
 
 from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
 from PySide6.QtCore import Slot
@@ -18,16 +20,45 @@ class MainWindow(QWidget):
         self.ui.pushButton_search.clicked.connect(self._search)
         self.ui.lineEdit_download_path.textChanged.connect(self._set_dl)
         self.ui.toolButton_download_path.clicked.connect(self._browse_dl)
-        # self.ui.lineEdit_search.textChange.connect(self._search)
-        # self.ui.lineEdit_url.setFocus()
 
     def load_ui(self):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+    def _validate_url(self, url):
+        code = urllib.request.urlopen(url).getcode()
+        if code == 200:
+            return True
+        logging.debug("{}: {}".format(code, type(code)))
+        return
+
+
     @Slot()
     def _search(self):
-        print(self.ui.lineEdit_search.text())
+        search_str = self.ui.lineEdit_search.text()
+        m3u = self.ui.lineEdit_url.text()
+        results = []
+        if not self._validate_url(m3u):
+            raise Exception("invalid m3u url")
+        with open(r"{}".format(m3u), 'r') as fp:
+            url_line_no = None
+            current_dict = {}
+            for l_no, line in enumerate(fp):
+                if url_line_no == l_no:
+                    current_dict["url"] = line.strip()
+                    results.append(current_dict)
+                    url_line_no = None
+                    current_dict = {}
+                    continue
+                if search_str in line.lower():
+                    split1 = line.split('tvg-name="')[1]
+                    split2 = split1.split('" tvg-logo="')
+                    logo = split2[1].split('" ')[0]
+                    title = split2[0]
+                    current_dict["title"] = title
+                    current_dict["logo"] = logo
+                    url_line_no = l_no + 1
+        return results
 
     @Slot()
     def _set_dl(self, dl_dir=None):
