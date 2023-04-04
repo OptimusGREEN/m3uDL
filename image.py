4502f862a831145ad5b85d1b6e89d74547eeae79
OptimusGREEN <johnmcbridegreen@gmail.com>
2023-04-04 21:45:38 +0100

    noImage url

--- image.py
+++ image.py
@@ -26,3 +26,6 @@ def imageFromUrl(url, *args, **kwargs):
     request = requests.get(url)
     pixmap.loadFromData(request.content)
     return pixmap
+
+def noImage():
+    return "https://st3.depositphotos.com/1037178/12744/v/600/depositphotos_127449166-stock-illustration-picture-icon-vector.jpg"
