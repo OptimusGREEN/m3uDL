9f410d1cf8a1299fc47b077ef3b3621be7986006
OptimusGREEN <johnmcbridegreen@gmail.com>
2023-04-06 23:13:36 +0100

    image.py cleanup

--- image.py
+++ image.py
@@ -1,25 +1,6 @@
 import sys
 import requests
-from PySide6.QtGui import QPixmap, QScreen
-from PySide6.QtWidgets import QApplication, QWidget, QLabel
-
-class imageBox(QLabel):
-    def __init__(self, image_file=None, url=None, *args, **kwargs):
-        super(imageBox, self).__init__()
-
-        # self.setScaledContents(True)
-
-        self.pixmap = QPixmap()
-        if url:
-            request = requests.get(url)
-            self.pixmap.loadFromData(request.content)
-            self.pixmap.scaled(self.size().width(),self.size().height())
-            self.setPixmap(self.pixmap)
-        elif image_file:
-            self.pixmap.loadFromData(image_file)
-            self.setPixmap(self.pixmap)
-        else:
-            pass
+from PySide6.QtGui import QPixmap
 
 def imageFromUrl(url, *args, **kwargs):
     pixmap = QPixmap()
